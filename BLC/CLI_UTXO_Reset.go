8b676122f3e915c68ea054aee75ac1838e183905
fishmc <278051663@qq.com>
2018-09-02 15:05:31 +0800

    ./bc

---
+++ BLC/CLI_UTXO_Reset.go
@@ -0,0 +1,13 @@
+package BLC
+
+func (cli *CLI) resetUTXOSet(nodeID string) {
+
+	blockchain := BlockchainObject(nodeID)
+
+	defer blockchain.DB.Close()
+
+	utxoSet := &UTXOSet{blockchain}
+
+	utxoSet.ResetUTXOSet()
+
+}
---
+++ BLC/CLI_printchain.go
@@ -0,0 +1,11 @@
+package BLC
+
+func (cli *CLI) printchain(nodeID string) {
+
+	blockchain := BlockchainObject(nodeID)
+
+	defer blockchain.DB.Close()
+
+	blockchain.Printchain()
+
+}
